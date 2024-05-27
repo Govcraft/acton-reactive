3e1aaeef4fbed235f770181bab84acda33b632c4
Roland Rodriguez <roland@govcraft.ai>
2024-05-27 16:56:06 -0600

    Add new license files and update copyright notices

--- akton-core/src/lib.rs
+++ akton-core/src/lib.rs
@@ -1,18 +1,32 @@
 /*
  *
+ *  *
  *  * Copyright (c) 2024 Govcraft.
  *  *
- *  * Licensed under the Apache License, Version 2.0 (the "License");
- *  * you may not use this file except in compliance with the License.
- *  * You may obtain a copy of the License at
+ *  *  Licensed under the Business Source License, Version 1.1 (the "License");
+ *  *  you may not use this file except in compliance with the License.
+ *  *  You may obtain a copy of the License at
  *  *
- *  *     http://www.apache.org/licenses/LICENSE-2.0
+ *  *      https://github.com/GovCraft/akton-framework/tree/main/LICENSES
  *  *
- *  * Unless required by applicable law or agreed to in writing, software
- *  * distributed under the License is distributed on an "AS IS" BASIS,
- *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  * See the License for the specific language governing permissions and
- *  * limitations under the License.
+ *  *  Change Date: Three years from the release date of this version of the Licensed Work.
+ *  *  Change License: Apache License, Version 2.0
+ *  *
+ *  *  Usage Limitations:
+ *  *    - You may use the Licensed Work for non-production purposes only, such as internal testing, development, and experimentation.
+ *  *    - You may not use the Licensed Work for any production or commercial purpose, including, but not limited to, the provision of any service to third parties, without a commercial use license from the Licensor, except as stated in the Exemptions section of the License.
+ *  *
+ *  *  Exemptions:
+ *  *    - Open Source Projects licensed under an OSI-approved open source license.
+ *  *    - Non-Profit Organizations using the Licensed Work for non-commercial purposes.
+ *  *    - Small For-Profit Companies with annual gross revenues not exceeding $2,000,000 USD.
+ *  *
+ *  *  Unless required by applicable law or agreed to in writing, software
+ *  *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  *  See the License for the specific language governing permissions and
+ *  *  limitations under the License.
+ *  *
  *
  *
  */
