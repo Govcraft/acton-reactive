3c27305172991ff6bdec5cc9c512f937d58bfdcb
Roland Rodriguez <roland@govcraft.ai>
2024-07-08 15:49:34 -0600

    feat(actor): implement Idle state
    This commit introduces the Idle state for ManagedActor. It includes the implementation of the Default trait for ManagedActor with Idle state. The Default implementation sets up the actor with default values and initializes the inbox and outbox channels, as well as various lifecycle hooks and reactors.
    
    NEW FEATURE: You appear to have introduced one or more new features that are backward-compatible. According to Semantic Versioning (SemVer), this requires a minor version update. Please verify and update your version number accordingly.

--- akton-core/src/actors/managed_actor/idle.rs
+++ akton-core/src/actors/managed_actor/idle.rs
@@ -1,1 +1,37 @@
+use std::fmt::Debug;
+
+use dashmap::DashMap;
+use tokio::sync::mpsc::channel;
+
+use crate::actors::ManagedActor;
+use crate::common::ActorRef;
+
 pub struct Idle;
+
+impl<ManagedEntity: Default + Send + Debug + 'static> Default for ManagedActor<Idle, ManagedEntity> {
+    fn default() -> Self {
+        let (outbox, inbox) = channel(255);
+        let mut actor_ref: ActorRef = Default::default();
+        actor_ref.outbox = Some(outbox.clone());
+
+        ManagedActor::<Idle, ManagedEntity> {
+            actor_ref,
+            parent: Default::default(),
+            key: Default::default(),
+            entity: ManagedEntity::default(),
+            broker: Default::default(),
+            inbox,
+            akton: Default::default(),
+            halt_signal: Default::default(),
+            tracker: Default::default(),
+            before_activate: Box::new(|_| {}),
+            on_activate: Box::new(|_| {}),
+            before_stop: Box::new(|_| {}),
+            on_stop: Box::new(|_| {}),
+            before_stop_async: None,
+            reactors: DashMap::new(),
+
+            _actor_state: Default::default(),
+        }
+    }
+}
