624b286ad0ae010a1665d8e2eb9ed40962fbd79b
Roland Rodriguez <roland@govcraft.ai>
2024-07-08 15:04:39 -0600

    refactor(actors)!: refactor ManagedActor
    Refactor ManagedActor to separate ActorState and ManagedEntity types. This includes renaming generic type parameters, modifying struct fields, and updating method signatures. Added new method from_idle for state transition from Idle to Awake. Improved lifecycle event handling and message processing.
    
    BREAKING CHANGE: Changes the generic type parameters and method signatures of ManagedActor.
    BREAKING CHANGES: You have made changes that may break backward compatibility. According to Semantic Versioning (SemVer), this requires a major version update. Please verify and update your version number accordingly.

--- akton-core/src/actors/managed_actor.rs
+++ akton-core/src/actors/managed_actor.rs
@@ -31,6 +31,7 @@
  *
  */
 
+use std::any::TypeId;
 use std::fmt;
 use std::fmt::Debug;
 use std::fmt::Formatter;
@@ -48,16 +49,14 @@ use tokio::time::timeout;
 use tokio_util::task::TaskTracker;
 use tracing::*;
 
-use crate::common::{ActorRef, Akton, AktonInner, BrokerRef, ParentRef, ReactorItem, ReactorMap, HaltSignal, SystemSignal};
-use crate::message::{BrokerRequestEnvelope, Envelope, OutboundEnvelope};
-use crate::prelude::AktonReady;
+use crate::common::{ActorRef, Akton, AktonInner, AsyncLifecycleHandler, BrokerRef, FutureBox, HaltSignal, IdleLifecycleHandler, LifecycleHandler, MessageHandler, ParentRef, ReactorItem, ReactorMap, SystemSignal};
+use crate::message::{BrokerRequestEnvelope, Envelope, EventRecord, OutboundEnvelope};
+use crate::prelude::{AktonMessage, AktonReady};
 use crate::traits::Actor;
 
 use super::{ActorConfig, Awake, Idle};
 
-pub struct ManagedActor<RefType: Send + 'static, ManagedEntity: Default + Send + Debug + 'static> {
-    pub setup: RefType,
-
+pub struct ManagedActor<ActorState, ManagedEntity: Default + Send + Debug + 'static> {
     pub actor_ref: ActorRef,
 
     pub parent: Option<ParentRef>,
@@ -74,16 +73,29 @@ pub struct ManagedActor<RefType: Send + 'static, ManagedEntity: Default + Send +
     pub(crate) tracker: TaskTracker,
 
     pub inbox: Receiver<Envelope>,
+    /// Reactor called before the actor wakes up.
+    pub(crate) before_activate: Box<IdleLifecycleHandler<Idle, ManagedEntity>>,
+    /// Reactor called when the actor wakes up.
+    pub(crate) on_activate: Box<LifecycleHandler<Awake, ManagedEntity>>,
+    /// Reactor called just before the actor stops.
+    pub(crate) before_stop: Box<LifecycleHandler<Awake, ManagedEntity>>,
+    /// Reactor called when the actor stops.
+    pub(crate) on_stop: Box<LifecycleHandler<Awake, ManagedEntity>>,
+    /// Asynchronous reactor called just before the actor stops.
+    pub(crate) before_stop_async: Option<AsyncLifecycleHandler<ManagedEntity>>,
+    /// Map of reactors for handling different message types.
+    pub(crate) reactors: ReactorMap<ManagedEntity>,
+    _actor_state: std::marker::PhantomData<ActorState>,
+
 }
 
-impl<ManagedEntity: Default + Send + Debug + 'static> Default for ManagedActor<Idle<ManagedEntity>, ManagedEntity> {
+impl<ManagedEntity: Default + Send + Debug + 'static> Default for ManagedActor<Idle, ManagedEntity> {
     fn default() -> Self {
         let (outbox, inbox) = channel(255);
         let mut actor_ref: ActorRef = Default::default();
         actor_ref.outbox = Some(outbox.clone());
 
-        ManagedActor {
-            setup: Idle::default(),
+        ManagedActor::<Idle, ManagedEntity> {
             actor_ref,
             parent: Default::default(),
             key: Default::default(),
@@ -93,12 +105,20 @@ impl<ManagedEntity: Default + Send + Debug + 'static> Default for ManagedActor<I
             akton: Default::default(),
             halt_signal: Default::default(),
             tracker: Default::default(),
+            before_activate: Box::new(|_| {}),
+            on_activate: Box::new(|_| {}),
+            before_stop: Box::new(|_| {}),
+            on_stop: Box::new(|_| {}),
+            before_stop_async: None,
+            reactors: DashMap::new(),
+
+            _actor_state: Default::default(),
         }
     }
 }
 
 
-impl<RefType: Send + 'static, State: Default + Send + Debug + 'static> Debug for ManagedActor<RefType, State> {
+impl<ActorState, ManagedEntity: Default + Send + Debug + 'static> Debug for ManagedActor<ActorState, ManagedEntity> {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         f.debug_struct("ManagedActor")
             .field("key", &self.key)
@@ -110,7 +130,7 @@ impl<RefType: Send + 'static, State: Default + Send + Debug + 'static> Debug for
 ///
 /// # Type Parameters
 /// - `State`: The type representing the state of the actor.
-impl<State: Default + Send + Debug + 'static> ManagedActor<Awake<State>, State> {
+impl<ManagedEntity: Default + Send + Debug + 'static> ManagedActor<Awake, ManagedEntity> {
     /// Creates a new outbound envelope for the actor.
     ///
     /// # Returns
@@ -139,8 +159,8 @@ impl<State: Default + Send + Debug + 'static> ManagedActor<Awake<State>, State> 
     }
 
     #[instrument(skip(reactors, self))]
-    pub(crate) async fn wake(&mut self, reactors: ReactorMap<State>) {
-        (self.setup.on_wake)(self);
+    pub(crate) async fn wake(&mut self, reactors: ReactorMap<ManagedEntity>) {
+        (self.on_activate)(self);
 
         while let Some(mut incoming_envelope) = self.inbox.recv().await {
             let type_id;
@@ -169,13 +189,13 @@ impl<State: Default + Send + Debug + 'static> ManagedActor<Awake<State>, State> 
                 self.terminate().await;
             }
         }
-        (self.setup.on_before_stop)(self);
-        if let Some(ref on_before_stop_async) = self.setup.on_before_stop_async {
+        (self.before_stop)(self);
+        if let Some(ref on_before_stop_async) = self.before_stop_async {
             if timeout(Duration::from_secs(5), on_before_stop_async(self)).await.is_err() {
                 tracing::error!("on_before_stop_async timed out or failed");
             }
         }
-        (self.setup.on_stop)(self);
+        (self.on_stop)(self);
     }
     #[instrument(skip(self))]
     async fn terminate(&mut self) {
@@ -193,7 +213,193 @@ impl<State: Default + Send + Debug + 'static> ManagedActor<Awake<State>, State> 
 ///
 /// # Type Parameters
 /// - `State`: The type representing the state of the actor.
-impl<ManagedEntity: Default + Send + Debug + 'static> ManagedActor<Idle<ManagedEntity>, ManagedEntity> {
+impl<ManagedEntity: Default + Send + Debug + 'static> ManagedActor<Idle, ManagedEntity> {
+    /// Adds a synchronous message handler for a specific message type.
+    ///
+    /// # Parameters
+    /// - `message_reactor`: The function to handle the message.
+    #[instrument(skip(self, message_handler))]
+    pub fn act_on<M: AktonMessage + Clone + 'static>(
+        &mut self,
+        message_handler: impl Fn(&mut ManagedActor<Awake, ManagedEntity>, &mut EventRecord<M>)
+        + Send
+        + Sync
+        + 'static,
+    ) -> &mut Self {
+        let type_id = TypeId::of::<M>();
+        trace!(type_name = std::any::type_name::<M>(), type_id = ?type_id);
+        // Create a boxed handler for the message type.
+        let handler: Box<MessageHandler<ManagedEntity>> = Box::new(
+            move |actor: &mut ManagedActor<Awake, ManagedEntity>, envelope: &mut Envelope| {
+                let envelope_type_id = envelope.message.as_any().type_id();
+                info!(
+                "Attempting to downcast message: expected_type_id = {:?}, envelope_type_id = {:?}",
+                type_id, envelope_type_id
+            );
+                if let Some(concrete_msg) = downcast_message::<M>(&*envelope.message) {
+                    let message = concrete_msg.clone();
+                    let sent_time = envelope.sent_time;
+                    let return_address = OutboundEnvelope::new(
+                        envelope.return_address.clone(),
+                        actor.key.clone(),
+                    );
+                    let event_record = &mut EventRecord {
+                        message,
+                        sent_time,
+                        return_address,
+                    };
+                    message_handler(actor, event_record);
+                    Box::pin(())
+                } else {
+                    Box::pin({
+                        error!(
+                        "Message type mismatch: expected {:?}",
+                        std::any::type_name::<M>()
+                    );
+                    })
+                };
+            },
+        );
+
+        // Insert the handler into the reactors map.
+        let _ = self.reactors.insert(type_id, ReactorItem::MessageReactor(handler));
+
+        self
+    }
+
+    /// Adds an asynchronous message handler for a specific message type.
+    ///
+    /// # Parameters
+    /// - `message_processor`: The function to handle the message.
+    #[instrument(skip(self, message_processor))]
+    pub fn act_on_async<M>(
+        &mut self,
+        message_processor: impl for<'a> Fn(&'a mut ManagedActor<Awake, ManagedEntity>, &'a mut EventRecord<M>) -> FutureBox
+        + Send
+        + Sync
+        + 'static,
+    ) -> &mut Self
+    where
+        M: AktonMessage + Clone + Send + Sync + 'static,
+    {
+        let type_id = TypeId::of::<M>();
+        trace!(type_name=std::any::type_name::<M>(),type_id=?type_id);
+        // Create a boxed handler for the message type.
+        let handler_box = Box::new(
+            move |actor: &mut ManagedActor<Awake, ManagedEntity>, envelope: &mut Envelope| -> FutureBox {
+                let envelope_type_id = envelope.message.as_any().type_id();
+                info!(
+                "Attempting to downcast message: expected_type_id = {:?}, envelope_type_id = {:?}",
+                type_id, envelope_type_id
+            );
+                if let Some(concrete_msg) = downcast_message::<M>(&*envelope.message) {
+                    info!("Message successfully downcasted to name {} and concrete type: {:?}",std::any::type_name::<M>(), type_id);
+
+                    let message = concrete_msg.clone();
+                    let sent_time = envelope.sent_time;
+                    let mut event_record = {
+                        if let Some(parent) = &actor.parent {
+                            let return_address = parent.return_address();
+                            EventRecord {
+                                message,
+                                sent_time,
+                                return_address,
+                            }
+                        } else {
+                            let return_address = actor.actor_ref.return_address();
+                            EventRecord {
+                                message,
+                                sent_time,
+                                return_address,
+                            }
+                        }
+                    };
+
+                    // Call the user-provided function and get the future.
+                    let user_future = message_processor(actor, &mut event_record);
+
+                    // Automatically box and pin the user future.
+                    Box::pin(user_future)
+                } else {
+                    error!(type_name=std::any::type_name::<M>(),"Should never get here, message failed to downcast");
+                    // Return an immediately resolving future if downcast fails.
+                    Box::pin(async {})
+                }
+            },
+        );
+
+        // Insert the handler into the reactors map.
+        let _ = &self
+            .reactors
+            .insert(type_id, ReactorItem::FutureReactor(handler_box));
+        self
+    }
+
+
+    /// Sets the reactor to be called before the actor wakes up.
+    ///
+    /// # Parameters
+    /// - `life_cycle_event_reactor`: The function to be called.
+    pub fn before_activate(
+        &mut self,
+        life_cycle_event_reactor: impl Fn(&ManagedActor<Idle, ManagedEntity>) + Send + Sync + 'static,
+    ) -> &mut Self {
+        self.before_activate = Box::new(life_cycle_event_reactor);
+        self
+    }
+
+    /// Sets the reactor to be called when the actor wakes up.
+    ///
+    /// # Parameters
+    /// - `life_cycle_event_reactor`: The function to be called.
+    pub fn on_activate(
+        &mut self,
+        life_cycle_event_reactor: impl Fn(&ManagedActor<Awake, ManagedEntity>) + Send + Sync + 'static,
+    ) -> &mut Self {
+        // Create a boxed handler that can be stored in the HashMap.
+        self.on_activate = Box::new(life_cycle_event_reactor);
+        self
+    }
+
+    /// Sets the reactor to be called when the actor stops.
+    ///
+    /// # Parameters
+    /// - `life_cycle_event_reactor`: The function to be called.
+    pub fn on_stop(
+        &mut self,
+        life_cycle_event_reactor: impl Fn(&ManagedActor<Awake, ManagedEntity>) + Send + Sync + 'static,
+    ) -> &mut Self {
+        // Create a boxed handler that can be stored in the HashMap.
+        self.on_stop = Box::new(life_cycle_event_reactor);
+        self
+    }
+
+    /// Sets the reactor to be called just before the actor stops.
+    ///
+    /// # Parameters
+    /// - `life_cycle_event_reactor`: The function to be called.
+    pub fn before_stop(
+        &mut self,
+        life_cycle_event_reactor: impl Fn(&ManagedActor<Awake, ManagedEntity>) + Send + Sync + 'static,
+    ) -> &mut Self {
+        // Create a boxed handler that can be stored in the HashMap.
+        self.before_stop = Box::new(life_cycle_event_reactor);
+        self
+    }
+
+    /// Sets the asynchronous reactor to be called just before the actor stops.
+    ///
+    /// # Parameters
+    /// - `f`: The asynchronous function to be called.
+    pub fn before_stop_async<F>(&mut self, f: F) -> &mut Self
+    where
+        F: for<'b> Fn(&'b ManagedActor<Awake, ManagedEntity>) -> FutureBox + Send + Sync + 'static,
+    {
+        self.before_stop_async = Some(Box::new(f));
+        self
+    }
+
+
     /// Creates and supervises a new actor with the given ID and state.
     ///
     /// # Parameters
@@ -205,7 +411,7 @@ impl<ManagedEntity: Default + Send + Debug + 'static> ManagedActor<Idle<ManagedE
     pub async fn create_child(
         &self,
         config: ActorConfig,
-    ) -> ManagedActor<Idle<ManagedEntity>, ManagedEntity> {
+    ) -> ManagedActor<Idle, ManagedEntity> {
         let actor = ManagedActor::new(&Some(self.akton.clone()), None, ManagedEntity::default()).await;
 
         event!(Level::TRACE, new_actor_key = &actor.key);
@@ -214,7 +420,7 @@ impl<ManagedEntity: Default + Send + Debug + 'static> ManagedActor<Idle<ManagedE
 
     #[instrument(skip(entity))]
     pub(crate) async fn new(akton: &Option<AktonReady>, config: Option<ActorConfig>, entity: ManagedEntity) -> Self {
-        let mut managed_actor: ManagedActor<Idle<ManagedEntity>, ManagedEntity> = ManagedActor::default();
+        let mut managed_actor: ManagedActor<Idle, ManagedEntity> = ManagedActor::default();
 
         if let Some(config) = &config {
             managed_actor.actor_ref.arn = config.name().clone();
@@ -237,10 +443,10 @@ impl<ManagedEntity: Default + Send + Debug + 'static> ManagedActor<Idle<ManagedE
 
     #[instrument(skip(self), fields(key = self.key))]
     pub async fn activate(mut self) -> ActorRef {
-        let reactors = mem::take(&mut self.setup.reactors);
+        let reactors = mem::take(&mut self.reactors);
         let actor_ref = self.actor_ref.clone();
 
-        let active_actor: ManagedActor<Awake<ManagedEntity>, ManagedEntity> = self.into();
+        let active_actor: ManagedActor<Awake, ManagedEntity> = self.from_idle();
         let actor = Box::leak(Box::new(active_actor));
 
         debug_assert!(!actor.inbox.is_closed(), "Actor mailbox is closed in activate");
@@ -250,4 +456,80 @@ impl<ManagedEntity: Default + Send + Debug + 'static> ManagedActor<Idle<ManagedE
 
         actor_ref
     }
+
+    #[instrument("from idle to awake", skip(self), fields(
+        key = self.key, children_in = self.actor_ref.children().len()
+    ))]
+    fn from_idle(self) -> ManagedActor<Awake, ManagedEntity>
+    where
+        ManagedEntity: Send + 'static,
+    {
+        tracing::trace!("*");
+        // Extract lifecycle reactors and other properties from the idle actor
+        let on_activate = self.on_activate;
+        let before_activate = self.before_activate;
+        let on_stop = self.on_stop;
+        let before_stop = self.before_stop;
+        let before_stop_async = self.before_stop_async;
+        let halt_signal = self.halt_signal;
+        let parent = self.parent;
+        let key = self.key;
+        let tracker = self.tracker;
+        let akton = self.akton;
+        let reactors = self.reactors;
+        // Trace the process and check if the mailbox is closed before conversion
+        tracing::trace!("Checking if mailbox is closed before conversion");
+        debug_assert!(
+            !self.inbox.is_closed(),
+            "Actor mailbox is closed before conversion in From<Actor<Idle, State>>"
+        );
+
+        let inbox = self.inbox;
+        let actor_ref = self.actor_ref;
+        let entity = self.entity;
+        let broker = self.broker;
+
+        // Trace the conversion process
+        // tracing::trace!(
+        //     "Converting Actor from Idle to Awake with key: {}",
+        //     key.self
+        // );
+        // tracing::trace!("Checking if mailbox is closed before conversion");
+        debug_assert!(
+            !inbox.is_closed(),
+            "Actor mailbox is closed in From<Actor<Idle, State>>"
+        );
+
+        // tracing::trace!("Mailbox is not closed, proceeding with conversion");
+        if actor_ref.children().is_empty() {
+            tracing::trace!(
+                    "child count before Actor creation {}",
+                    actor_ref.children().len()
+                );
+        }
+        // Create and return the new actor in the awake state
+        ManagedActor::<Awake, ManagedEntity>{
+            actor_ref,
+            parent,
+            halt_signal,
+            key,
+            akton,
+            entity,
+            tracker,
+            inbox,
+            before_activate,
+            on_activate,
+            before_stop,
+            on_stop,
+            before_stop_async,
+            broker,
+            reactors,
+            _actor_state: Default::default(),
+        }
+    }
+}
+
+// Function to downcast the message to the original type.
+pub fn downcast_message<T: 'static>(msg: &dyn AktonMessage) -> Option<&T> {
+    msg.as_any().downcast_ref::<T>()
 }
