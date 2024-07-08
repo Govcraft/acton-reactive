400884646fe07a4096f9ee49f556c6b334e3b8d6
Roland Rodriguez <roland@govcraft.ai>
2024-07-08 15:33:37 -0600

    style(logging): replace trace with debug logs
    Replaces trace level logging with debug level logging for message type name and message type ID. This change affects both the message broadcasting and subscriber addition sections of the Broker implementation.

--- akton-core/src/common/broker.rs
+++ akton-core/src/common/broker.rs
@@ -35,7 +35,7 @@ impl Broker {
                 let message = event.message.clone();
                 let message_type_id = (event.message).message.as_ref().type_id();
                 let message_type_name = event.message.message_type_name.clone();
-                trace!(message_type_name=message_type_name, message_type_id=?message_type_id);
+                debug!(message_type_name=message_type_name, message_type_id=?message_type_id);
 
                 Box::pin(async move {
                     Broker::broadcast(subscribers, message).await;
@@ -53,7 +53,7 @@ impl Broker {
                         .entry(message_type_id)
                         .or_insert_with(HashSet::new)
                         .insert((subscriber_id.clone(), subscriber_context.clone()));
-                    trace!(message_type_name=message_type_name, message_type_id=?message_type_id, subscriber=subscriber_context.arn, "Subscriber added");
+                    debug!(message_type_name=message_type_name, message_type_id=?message_type_id, subscriber=subscriber_context.arn, "Subscriber added");
                 })
             });
 
