b317fde32792f1cb371e030f5f494e9f87f82f40
Roland Rodriguez <roland@govcraft.ai>
2024-07-08 13:27:49 -0600

    fix(actors): correct reactor item type
    Corrects the type of reactor item being inserted into the reactors map from ReactorItem::Future to ReactorItem::FutureReactor. This ensures the correct handling of the reactor item within the Idle actor.
    
    BUG FIX: You appear to have made one or more backward-compatible bug fixes. According to Semantic Versioning (SemVer), this requires a patch version update. Please verify and update your version number accordingly.

--- akton-core/src/actors/idle.rs
+++ akton-core/src/actors/idle.rs
@@ -208,7 +208,7 @@ impl<State: Default + Send + Debug> Idle<State> {
         // Insert the handler into the reactors map.
         let _ = &self
             .reactors
-            .insert(type_id, ReactorItem::Future(handler_box));
+            .insert(type_id, ReactorItem::FutureReactor(handler_box));
         self
     }
 
