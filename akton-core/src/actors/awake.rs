c08a783d1f91a542a3a05ae37c6345d23046fc00
Roland Rodriguez <roland@govcraft.ai>
2024-07-08 15:04:12 -0600

    refactor(actors): simplify Awake struct and conversion
    Removed commented-out code and simplified the Awake struct by consolidating the lifecycle handlers. The Debug implementation for Awake struct remains unchanged. The conversion from ManagedActor<Idle<State>, State> to ManagedActor<Awake<State>, State> has been refactored to streamline the process and ensure proper mailbox state checks.

--- akton-core/src/actors/awake.rs
+++ akton-core/src/actors/awake.rs
@@ -46,115 +46,97 @@ use crate::traits::Actor;
 ///
 /// # Type Parameters
 /// - `State`: The type representing the state of the actor.
-pub struct Awake<State: Default + Send + Debug + 'static> {
-    /// Reactor called when the actor wakes up.
-    pub(crate) on_wake: Box<LifecycleHandler<Awake<State>, State>>,
-    /// Reactor called just before the actor stops.
-    pub(crate) on_before_stop: Box<LifecycleHandler<Awake<State>, State>>,
-    /// Asynchronous reactor called just before the actor stops.
-    pub(crate) on_before_stop_async: Option<AsyncLifecycleHandler<State>>,
-    /// Reactor called when the actor stops.
-    pub(crate) on_stop: Box<LifecycleHandler<Awake<State>, State>>,
-}
+pub struct Awake;
 
-/// Custom implementation of the `Debug` trait for the `Awake` struct.
-///
-/// This implementation provides a formatted output for the `Awake` struct.
-impl<State: Default + Send + Debug + 'static> Debug for Awake<State> {
-    /// Formats the `Awake` struct using the given formatter.
-    ///
-    /// # Parameters
-    /// - `f`: The formatter used for writing formatted output.
-    ///
-    /// # Returns
-    /// A result indicating whether the formatting was successful.
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        f.debug_struct("Awake").finish()
-    }
-}
 
-/// Conversion from `Actor<Idle<State>, State>` to `Actor<Awake<State>, State>`.
-///
-/// This implementation provides a way to transition an actor from the idle state to the awake state.
-///
-/// # Type Parameters
-/// - `State`: The type representing the state of the actor.
-impl<State: Default + Send + Debug + 'static> From<ManagedActor<Idle<State>, State>>
-for ManagedActor<Awake<State>, State>
-{
-    /// Converts an `Actor` from the idle state to the awake state.
-    ///
-    /// # Parameters
-    /// - `value`: The `Actor` instance in the idle state.
-    ///
-    /// # Returns
-    /// A new `Actor` instance in the awake state.
-    #[instrument("from idle to awake", skip(value), fields(
-        key = value.key, children_in = value.actor_ref.children().len()
-    ))]
-    fn from(value: ManagedActor<Idle<State>, State>) -> ManagedActor<Awake<State>, State>
-    where
-        State: Send + 'static,
-    {
-        tracing::trace!("*");
-        // Extract lifecycle reactors and other properties from the idle actor
-        let on_wake = value.setup.on_activate;
-        let on_stop = value.setup.on_stop;
-        let on_before_stop = value.setup.before_stop;
-        let on_before_stop_async = value.setup.before_stop_async;
-        let halt_signal = value.halt_signal;
-        let parent_return_envelope = value.parent;
-        let key = value.key;
-        let task_tracker = value.tracker;
-        let akton = value.akton;
-
-        // Trace the process and check if the mailbox is closed before conversion
-        tracing::trace!("Checking if mailbox is closed before conversion");
-        debug_assert!(
-            !value.inbox.is_closed(),
-            "Actor mailbox is closed before conversion in From<Actor<Idle<State>, State>>"
-        );
-
-        let mailbox = value.inbox;
-        let context = value.actor_ref;
-        let state = value.entity;
-        let broker = value.broker;
-
-        // Trace the conversion process
-        // tracing::trace!(
-        //     "Converting Actor from Idle to Awake with key: {}",
-        //     key.value
-        // );
-        // tracing::trace!("Checking if mailbox is closed before conversion");
-        debug_assert!(
-            !mailbox.is_closed(),
-            "Actor mailbox is closed in From<Actor<Idle<State>, State>>"
-        );
-
-        // tracing::trace!("Mailbox is not closed, proceeding with conversion");
-        if context.children().is_empty() {
-            tracing::trace!(
-                "child count before Actor creation {}",
-                context.children().len()
-            );
-        }
-        // Create and return the new actor in the awake state
-        ManagedActor {
-            setup: Awake {
-                on_wake,
-                on_before_stop,
-                on_before_stop_async,
-                on_stop,
-            },
-            actor_ref: context,
-            parent: parent_return_envelope,
-            halt_signal,
-            key,
-            akton,
-            entity: state,
-            tracker: task_tracker,
-            inbox: mailbox,
-            broker,
-        }
-    }
-}
+// /// Conversion from `Actor<Idle, State>` to `Actor<Awake<State>, State>`.
+// ///
+// /// This implementation provides a way to transition an actor from the idle state to the awake state.
+// ///
+// /// # Type Parameters
+// /// - `State`: The type representing the state of the actor.
+// impl From<ManagedActor<Idle, State>>
+// for ManagedActor<Awake<State>, State>
+// {
+//     /// Converts an `Actor` from the idle state to the awake state.
+//     ///
+//     /// # Parameters
+//     /// - `value`: The `Actor` instance in the idle state.
+//     ///
+//     /// # Returns
+//     /// A new `Actor` instance in the awake state.
+//     #[instrument("from idle to awake", skip(value), fields(
+//         key = value.key, children_in = value.actor_ref.children().len()
+//     ))]
+//     fn from(value: ManagedActor<Idle, State>) -> ManagedActor<Awake<State>, State>
+//     where
+//         State: Send + 'static,
+//     {
+//         tracing::trace!("*");
+//         // Extract lifecycle reactors and other properties from the idle actor
+//         let on_wake = value.on_activate;
+//         let on_stop = value.on_stop;
+//         let on_before_stop = value.before_stop;
+//         let on_before_stop_async = value.before_stop_async;
+//         let halt_signal = value.halt_signal;
+//         let parent_return_envelope = value.parent;
+//         let key = value.key;
+//         let task_tracker = value.tracker;
+//         let akton = value.akton;
+//
+//         // Trace the process and check if the mailbox is closed before conversion
+//         tracing::trace!("Checking if mailbox is closed before conversion");
+//         debug_assert!(
+//             !value.inbox.is_closed(),
+//             "Actor mailbox is closed before conversion in From<Actor<Idle, State>>"
+//         );
+//
+//         let mailbox = value.inbox;
+//         let context = value.actor_ref;
+//         let state = value.entity;
+//         let broker = value.broker;
+//
+//         // Trace the conversion process
+//         // tracing::trace!(
+//         //     "Converting Actor from Idle to Awake with key: {}",
+//         //     key.value
+//         // );
+//         // tracing::trace!("Checking if mailbox is closed before conversion");
+//         debug_assert!(
+//             !mailbox.is_closed(),
+//             "Actor mailbox is closed in From<Actor<Idle, State>>"
+//         );
+//
+//         // tracing::trace!("Mailbox is not closed, proceeding with conversion");
+//         if context.children().is_empty() {
+//             tracing::trace!(
+//                 "child count before Actor creation {}",
+//                 context.children().len()
+//             );
+//         }
+//         // Create and return the new actor in the awake state
+//         // ManagedActor {
+//         //     // setup: Awake {
+//         //     //     on_wake,
+//         //     //     on_before_stop,
+//         //     //     on_before_stop_async,
+//         //     //     on_stop,
+//         //     // },
+//         //     actor_ref: context,
+//         //     parent: parent_return_envelope,
+//         //     halt_signal,
+//         //     key,
+//         //     akton,
+//         //     entity: state,
+//         //     tracker: task_tracker,
+//         //     inbox: mailbox,
+//         //     before_activate: Box::new(()),
+//         //     on_activate: on_wake,
+//         //     before_stop: on_before_stop,
+//         //     on_stop,
+//         //     before_stop_async: on_before_stop_async,
+//         //     broker,
+//         //     reactors: Default::default(),
+//         // }
+//     }
+// }
