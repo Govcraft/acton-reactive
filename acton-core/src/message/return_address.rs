9f6cd9a1d299009dd983537910be9add1e27e819
Roland Rodriguez <roland@govcraft.ai>
2024-07-09 02:52:01 -0600

    fix(message): fix default impl
    Fix the Default implementation for ReturnAddress struct. Replace the todo!() macro with actual initialization logic. Use tokio::sync::mpsc::channel to create an Outbox and initialize the struct with Ern::default().
    
    BUG FIX: You appear to have made one or more backward-compatible bug fixes. According to Semantic Versioning (SemVer), this requires a patch version update. Please verify and update your version number accordingly.

--- acton-core/src/message/return_address.rs
+++ acton-core/src/message/return_address.rs
@@ -1,7 +1,11 @@
+use std::sync::mpsc;
+use std::sync::mpsc::{channel, Sender};
+
 use acton_ern::prelude::*;
-use crate::common::Outbox;
-use crate::message::Envelope;
 use derive_new::new;
+
+use crate::common::{Envelope, Outbox};
+
 #[derive(new, Clone, Debug)]
 pub struct ReturnAddress {
     pub address: Outbox,
@@ -10,6 +14,7 @@ pub struct ReturnAddress {
 
 impl Default for ReturnAddress {
     fn default() -> Self {
-        todo!()
+        let (outbox, _) = tokio::sync::mpsc::channel(1);
+        Self::new(outbox, Ern::default())
     }
 }
