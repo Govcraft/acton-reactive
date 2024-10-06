9ff318d35cc90f5d97087b5a25f48163b192dcb1
Roland Rodriguez <roland@govcraft.ai>
2024-10-06 09:23:12 -0600

    feat(cli): experiment cli

---
+++ acton-cli/src/agents.rs
@@ -0,0 +1,5 @@
+mod scaffold_agent;
+mod view_manager;
+
+pub use view_manager::ViewManager;
+pub use scaffold_agent::ScaffoldAgent;
--- flake.nix
+++ flake.nix
@@ -7,27 +7,28 @@
   };
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in {
-        devShell = pkgs.mkShell {
-          buildInputs = [
-            pkgs.rustup
-            pkgs.cargo
-            pkgs.rust-analyzer
-            pkgs.clippy
-            pkgs.cargo-tarpaulin
-            pkgs.cargo-release
-            pkgs.cargo-machete
-            pkgs.rustfmt
-            pkgs.pkg-config # Optional
-          ];
+  flake-utils.lib.eachDefaultSystem (system:
+  let
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    devShell = pkgs.mkShell {
+      buildInputs = [
+        pkgs.rustup
+        pkgs.cargo
+        pkgs.jetbrains.rust-rover # Direct access to rust-rover in the right path
+        pkgs.rust-analyzer
+        pkgs.clippy
+        pkgs.cargo-tarpaulin
+        pkgs.cargo-release
+        pkgs.cargo-machete
+        pkgs.rustfmt
+        pkgs.pkg-config # Optional
+      ];
 
-          shellHook = ''
-            rustup default stable
-          '';
-        };
-      });
+      shellHook = ''
+        rustup default stable
+      '';
+    };
+  });
 }
 
