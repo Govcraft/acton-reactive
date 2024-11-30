5f472f90f747db0a568f7a08b5ae0acbbf733765
Roland Rodriguez <roland@govcraft.ai>
2024-11-30 14:41:02 -0600

    chore: add vhs for demo gifs

--- flake.nix
+++ flake.nix
@@ -6,34 +6,42 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-  flake-utils.lib.eachDefaultSystem (system:
-  let
-    pkgs = import nixpkgs {
-      inherit system;
-      config.allowUnfree = true; # Allow unfree packages
-    };
-  in {
-    devShell = pkgs.mkShell {
-      buildInputs = [
-        pkgs.rustup
-        pkgs.cargo
-        pkgs.llvm
-        pkgs.jetbrains.rust-rover
-        pkgs.rust-analyzer
-        pkgs.clippy
-        pkgs.cargo-tarpaulin
-        pkgs.cargo-release
-        pkgs.tokei
-        pkgs.cargo-machete
-        pkgs.rustfmt
-        pkgs.pkg-config # Optional
-      ];
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true; # Allow unfree packages
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.rustup
+            pkgs.cargo
+            pkgs.llvm
+            pkgs.jetbrains.rust-rover
+            pkgs.rust-analyzer
+            pkgs.clippy
+            pkgs.cargo-tarpaulin
+            pkgs.cargo-release
+            pkgs.tokei
+            pkgs.vhs
+            pkgs.cargo-machete
+            pkgs.rustfmt
+            pkgs.pkg-config # Optional
+          ];
 
-      shellHook = ''
-        rustup default stable
-      '';
-    };
-  });
+          shellHook = ''
+            rustup default stable
+          '';
+        };
+      }
+    );
 }
-
