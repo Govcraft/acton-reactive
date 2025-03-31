ca51e277301e49865f9dc3168af32c2d76bbc017
Roland Rodriguez <roland@govcraft.ai>
2025-03-31 12:57:18 -0500

    refactor(reactive): update crate root (lib.rs)

--- acton-reactive/src/lib.rs
+++ acton-reactive/src/lib.rs
@@ -13,7 +13,6 @@
  * See the applicable License for the specific language governing permissions and
  * limitations under that License.
  */
-#![forbid(unsafe_code)]
 #![forbid(unused)]
 
 //! Acton Main Library
