2b64b7ec0440265fbb878227ebb57c46272fc778
Roland Rodriguez <roland@govcraft.ai>
2025-12-04 16:40:37 -0600

    docs(docs-site): update version to 7.0.0

--- acton-docs-site/src/lib/version.ts
+++ acton-docs-site/src/lib/version.ts
@@ -2,4 +2,4 @@
  * Version information for acton-reactive
  * This should match the version in the root Cargo.toml workspace.package.version
  */
-export const VERSION = '0.7.0'
+export const VERSION = '7.0.0'
